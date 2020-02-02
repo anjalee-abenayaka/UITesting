8b0408fe0dd0d5b3cf95366bc2f5062c532d1945
anjalee <anjalee@users.noreply.github.com>
2020-02-02 20:30:32 +0530

    Initial Commit

---
+++ UITesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITesting
+//
+//  Created by Anjalee on 2/2/20.
+//  Copyright © 2020 Anjalee Abenayaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
